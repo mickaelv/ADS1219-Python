661f8561333cfbad907f1454878c69ba7b32bddb
mickaelv <mickael@miniworld-lyon.com>
2020-10-25 22:05:13 +0100

    Adding all functions. Before testing...

--- ADS1219/ADS1219.py
+++ ADS1219/ADS1219.py
@@ -123,3 +123,216 @@ class ADS1219:
 		return list( read )
 
 
+	def start( self ):
+		"""Start the conversion"""
+		self.__write_command( self.COMMAND_START_SYNC )
+
+	def resetConfig( self ):
+		"""Reset Chip"""
+		self.config = 0x00
+		self.__write_command( self.COMMAND_RESET )
+
+	def poweroff( self ):
+		"""Power off the Chip"""
+		self.__write_command( self.COMMAND_POWERDOWN )
+
+
+	def __sendConfig ( self ):
+		"""Send  config to the chip """
+		self.bus.write_byte_data( self.i2c_adr, self.COMMAND_WREG, self.config )
+
+	def setGain( self, gain ):
+		"""
+		Define the gain
+
+		Parameters
+		----------
+		gain : int
+ 		 	value of the gain:  1 or 4
+
+		:Example:
+
+			ADS1219.setGain(4) 
+
+		"""
+		self.config &= GAIN_MASK
+		if ( gain == 4) : self.config |= GAIN_4
+		elif( gain == 1) : self.config |= GAIN_1
+		else : raise ValueError("'gain' can only be either 1 or 4")
+		self.__sendConfig()
+		
+	def setDataRate( self, datarate ):
+		"""
+		Define the datarate
+
+		Parameters
+		----------
+		datarate : int
+ 		 	value of the datarate:  20, 90, 330 or 1000
+		
+		size : int
+			size of data
+
+		:Example:
+
+			ADS1219.setDataRate(1000) 
+
+		"""
+
+		self.config &= DATA_RATE_MASK
+		if( datarate == 20 ) : self.config |= DATA_RATE_20
+		elif( datarate == 90 ) : self.config |= DATA_RATE_90
+		elif( datarate == 330 ) : self.config |= DATA_RATE_330
+		elif( datarate == 1000 ) : self.config |= DATA_RATE_1000
+		else : raise ValueError("'datarate' can only be either 20, 90, 330 or 1000")
+		self.__sendConfig()
+
+
+	def setSingleShot( self ):
+		"""Configure the chip in Single Shot mode """
+		self.config &= MODE_MASK
+		self.config |= MODE_SINGLESHOT
+		self.__sendConfig()
+
+	def setContinuous( self ):
+		"""Configure the chip in Continuous mode """
+		self.config &= MODE_MASK
+		self.config |= MODE_CONTINUOUS
+		self.__sendConfig()
+
+	def setExternalReference( self ):
+		"""Configure the chip with an external reference """
+		self.config &= VREF_MASK
+		self.config |= VREF_EXTERNAL
+		self.__sendConfig()
+
+	def setInternalReference( self ):
+		"""Configure the chip with the internal reference """
+		self.config &= VREF_MASK
+		self.config |= VREF_INTERNAL
+		self.__sendConfig()
+
+	def readSigleEnded( self, channel ):
+		"""
+		Read a value from a specific channel
+
+		Parameters
+		----------
+		channel : int
+			channel number:  0 to 3
+
+		:Example:
+
+			ADS1219.readSingleEnded(0)
+
+		"""	
+		self.config &= MUX_MASK
+		if( channel == 0 ) : self.config |= MUX_SINGLE_0
+		elif( channel == 1 ) : self.config |= MUX_SINGLE_1
+		elif( channel == 2 ) : self.config |= MUX_SINGLE_2
+		elif( channel == 3 ) : self.config |= MUX_SINGLE_3
+		else : raise ValueError("'channel' can only be either 0, 1, 2 or 3")
+
+
+	def readConversionResult( self ):
+		""" Get the result data from the chip """
+		buffer = self.read_registers( self.COMMAND_RDATA, 3 )
+		value = (buffer[0]<<16) | (buffer[1]<<8) | (buffer[2])
+		if value >= 0x800000:
+			value = value-0x1000000
+		return value
+
+	def isReady( self ):
+		""" Software function to get the moment who the data is available """
+		value = self.read_registers( self.COMMAND_RREG|4, 1 )
+		return value[0] & 0x80
+
+
+	def waitResult( self ):
+		"""Blocking fonction to wait data"""
+		if self.readyPin>0:
+			GPIO.wait_for_edge( self.readyPin, GPIO.FALLING )
+		else:
+			while not self.isReady():
+				sleep(0.0001)
+
+
+
+	def readDifferential_0_1( self ):
+		"""Read a value between 0 and 1 channel"""	
+		self.config &= MUX_MASK
+		self.config |= MUX_DIF_0_1
+		self.__sendConfig()
+		self.start()
+		self.waitResult()
+		return self.readConversionResult()
+
+	def readDifferential_1_2( self ):
+		"""Read a value between 1 and 2 channel"""	
+		self.config &= MUX_MASK
+		self.config |= MUX_DIF_1_2
+		self.__sendConfig()
+		self.start()
+		self.waitResult()
+		return self.readConversionResult()
+
+	def readDifferential_2_3( self ):
+		"""Read a value between 2 and 3 channel"""	
+		self.config &= MUX_MASK
+		self.config |= MUX_DIF_2_3
+		self.__sendConfig()
+		self.start()
+		self.waitResult()
+		return self.readConversionResult()
+
+
+	def readShorted( self ):
+		"""
+		Set the chip to get an offset to calibrate
+
+		.. note:: Please refer to 8.3.7 in the datasheet ( https://www.ti.com/lit/ds/sbas924a/sbas924a.pdf )
+
+		"""	
+		self.config &= MUX_MASK
+		self.config |= MUX_SHORTED
+		self.__sendConfig()
+		self.start()
+		self.waitResult()
+		return self.readConversionResult()
+
+	def __init__( self, port=1, address=0x40, readyPin=0 ):
+		"""
+		Constructor 
+		Parameters
+		----------
+		port : int
+			I2C port, for raspberry with port 2 and 3 please set to port=1
+		address : hex		
+			I2C adress of the chip. Default is 0x40
+		readyPin: int
+			Input pin of the raspberry to know if data is available. 0 to desactivate and use software information
+		:Example:
+
+			ADS1219 = new ADS1219(1, 0x40, 3)
+
+		"""
+		self.i2c_adr = address 
+		self.bus = SMBus( port, True )
+		self.readyPin = readyPin
+		self.config = 0x00
+		if( readyPin>0 ):
+			GPIO.setmode( GPIO.BCM )
+			GPIO.setup(readyPin, GPIO.IN )
+
+	def __enter__( self ):
+		"""Enter function"""
+		return self
+		
+	def __del__( self ):
+		"""Delete function"""
+		self.bus.close()
+
+	def __exit__( self, exc_type, exc_value, traceback ):
+		"""Exit function"""
+		self.bus.close()
+
